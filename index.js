a02d0b4e4c95dade3ca5f46d3103b69063b8881d
rohit prajapat <rohitprajapat9039@gmail.com>
2022-07-11 15:52:27 +0530

    hello all

---
+++ index.js
@@ -0,0 +1,177 @@
+// console.log("hello world");
+// console.log(34, true, "rohit is bad");
+// document.write("this is a document");
+// // alert("me");
+
+// // console api
+// console.error("this is an error");
+// console.warn("this is warning");
+// console.log("this is good");
+// // variable
+// var number1 = 34;
+// var number2 = 56;
+// console.log(number1 + number2);
+// document.write(number1)
+
+// var str1 = ("this is good because is not to learn what you do");
+// var str2 = ("but there are a person who to provide thing")
+// console.log(str1 + str2);
+// // object
+// var rohit = {
+//     maths: 45,
+//     physics: 67,
+//     english: 34
+// }
+// console.log(rohit);
+// var string = {
+//     1: ("this is rohit "),
+//     2: ("this is nitesh"),
+//     3: ("there")
+// }
+// console.log(string);
+// // Boolean
+// var a = true;
+// var b = false
+// console.log(a, b);
+// // null,undefined
+// var c =
+//     console.log(c);
+// var d = null;
+// console.log(d);
+// // arrays
+// let ro = [2945, 555, "VIP", 79];
+// // ro.pop();
+// ro.push("rohit");
+// ro.shift();
+// ro.unshift("yuvi");
+// ro.toString()
+// console.log(ro);
+// /*operators in javascript 
+//  1. arithmatic
+//  2. assignment*/
+// var a = 100;
+// var b = 10;
+// console.log("the value of a + b is ", a + b);
+// console.log("the value of a - b is ", a - b);
+// console.log("the value of a * b is ", a * b);
+// console.log("the value of a / b is ", a / b);
+// var c = 24;
+// var d = 38;
+// var e = 39;
+// var f = 9999
+// c *= 2;
+// d -= 4;
+// e *= 4;
+// f = f + 1;
+// console.log(c, d, e, f);
+// // logical operator
+// // logical and
+
+// console.log(true && false);
+// console.log(false && false);
+// console.log(true && true);
+// console.log(false && true);
+// // logical or
+// console.log(true || false);
+// console.log(true || true);
+// console.log(false || false);
+// console.log(false || true);
+// // logical not
+// console.log(!true);
+// console.log(!false);
+// // comparion operator
+// a = 34;
+// b = 66;
+// console.log(a == b);
+// console.log(a > b);
+// console.log(a < b);
+
+// /*function in javascript*/
+// function avg(a, b) {
+//     c = (a + b) / 2;
+//     return c
+// }
+// c1 = avg(34, 66)
+// c2 = avg(45, 45)
+// console.log(c1, c2);
+
+// // conditionals in java script
+// var age = 19;
+// if (age > 18) {
+//     document.write('you can drink rasna water')
+// }
+// if (age > 18) {
+//     console.log('you can drink rasna water')
+// }
+// else { console.log('you are stupied') }
+
+// // if else statement
+// var age = 80;
+// if (age < 15) {
+//     console.log("there is person who angry every time")
+// }
+// else if (age < 20) {
+//     console.log("good person")
+// }
+// else if (age < 45) {
+//     console.log("old person")
+// }
+// else if (age < 80) {
+//     console.log("died person")
+// }
+// else {
+//     console.log("ro hit men")
+// }
+// let arr = [1, 2, 3, 4, 5, 6, 7];
+// // loop
+// console.log(arr)
+// for (var i = 0; i < arr.length; i++) {
+//     if (i == 2) {
+//         break;
+//         // continue;
+//     }
+//     console.log(arr[i]);
+// }
+// // arr.forEach(function (element) {
+// // console.log(element);})
+// // const ac = 0;
+// // ac++;
+// /*
+// let h = 0;
+// while (h < arr.length) {
+//     console.log(arr[h]);
+//     h++;
+// }*/
+// // let j = 0;
+// // do {
+// //     console.log(arr[j]);
+// //     j++;}
+// // while(j<arr.length);
+// let cat = "yuvraj is one of my favourite cricketer";
+// console.log(cat.length);
+// console.log(cat.indexOf("my"));
+// console.log(cat.slice(0, 11));
+// d = cat.replace("yuvraj","steve smith")
+// console.log(d,cat)
+// let mydate = new Date();
+// console.log(mydate);
+// console.log(mydate.getMinutes())
+// console.log(mydate.getSeconds())
+// console.log(mydate.getTime())
+// console.log(mydate.getDay())
+let elem = document.getElementById("pro");
+console.log(elem)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
